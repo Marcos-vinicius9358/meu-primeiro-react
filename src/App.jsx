4db2a9dc769debf31d6069be1d839b2d6cf67a72
Marcos Vinícius <marcosmeireles9358@gmail.com>
2026-04-22 11:55:52 -0400

    cria header

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Botao from './components/Botao/Botao'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>
+    <Header/>
       <h1>eu sou o app</h1>
       <Botao texto="vamos" />
       <Botao texto="oi"/>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css"
+
+const Header = () => {
+  return (
+    <header>
+
+      <ul>
+        <li>teste</li>
+        <li>teste</li>
+        <li>teste</li>
+      </ul>
+    </header>
+  )
+}
+
+
+export default Header;
