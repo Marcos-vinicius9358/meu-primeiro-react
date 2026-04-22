cefe2cc87323406088357896a6cdbd34a4300213
Marcos Vinícius <marcosmeireles9358@gmail.com>
2026-04-22 11:42:04 -0400

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Botao from './components/Botao/Botao'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>eu sou o app</h1>
+      <Botao texto="vamos" />
+      <Botao texto="oi"/>
+      <Botao texto="oo"/>
+      <Botao texto="vamo dormi"/>
+      <Botao />
+      <Botao />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Botao/Botao.jsx
@@ -0,0 +1,10 @@
+import  "./Botao.css"
+
+const Botao = ({texto}) =>{
+
+return(
+<button>{texto}</button>
+)
+}
+
+export default Botao
